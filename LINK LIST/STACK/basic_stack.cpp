80fc6cfb4d594ab15fc3f2b814105662f314bcba
redowan-dot <redowan12521126@gmail.com>
2026-07-06 16:37:09 +0600

    stack

---
+++ LINK LIST/STACK/basic_stack.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    stack<int> st;
+
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    while (!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+
+    return 0;
+}
