dfe4c918907347dc0741ff13e8250a4fbae12c08
redowan-dot <redowan12521126@gmail.com>
2026-07-20 22:30:44 +0600

    insert at head

---
+++ LINK LIST/insert_at_head_doubly.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+
+void insert_at_head(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+
+
+void print(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int x;
+
+    while (true)
+    {
+        cin >> x;
+
+        if (x == -1)
+            break;
+
+        insert_at_head(head, tail, x);
+    }
+
+    print(head);
+
+    return 0;
+}
